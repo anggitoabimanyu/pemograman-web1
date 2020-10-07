d3cfa2ba95e1f4a851161e4b6ced04b9cf69fbd8
anggitoabimanyu <anggitoabimanyu00@gmail.com>
2020-10-07 14:33:22 +0700

    konfirmasi

---
+++ pertemuan4/konfirmasi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konfirmasi</title>
+</head>
+<body>
+<p>Konfirmasi System</p>
+    <form method="post" action="konfirmasi.php">
+    <label>Nama Lengkap</label>
+    <input class="inputan" type="text" name="namalengkap">
+
+    <label>Password</label>
+    <input class="inputan" type="password" name="password">
+</body>
+</html>
